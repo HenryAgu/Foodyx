7d1e07a37ca1b08cde2c197c7300f8e106e992e1
Henry <aguhenrychuks@gmail.com>
2022-12-02 23:13:27 +0100

    feat: navbar set, home set aside swiper

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,33 @@
-import './Navbar.css'
+// stylesheet
+import "./Navbar.css";
+
+// Navlink
+import { NavLink } from "react-router-dom";
+
+// Images
+import logo from "../Images/Logo.svg";
+import globe from "../Images/Globe.svg";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <div className="navbar">
+        <header>
+          <div className="logo">
+            <NavLink to="/">
+              <img src={logo} alt="logo" />
+            </NavLink>
+          </div>
+          <div className="left-side">
+            <img src={globe} alt="globe" />
+            <NavLink to="/">
+              <button>Get started</button>
+            </NavLink>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
