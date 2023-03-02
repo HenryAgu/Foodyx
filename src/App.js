d43edbb22900cf5b409cd6a041710a0445dc6109
Henry <aguhenrychuks@gmail.com>
2023-03-02 10:26:22 +0100

    fix: scroll to top

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Home from '../src/Pages/Home';
 import CaloriesPage from '../src/Pages/CaloriesPage';
+import ScrollToTop from './Components/ScrollToTop';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <ScrollToTop/>
       <Navbar/>
       <Routes>
           <Route exact path="/" element={<Home/>}/>
